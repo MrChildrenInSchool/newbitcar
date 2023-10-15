4dac6971a295aa49db8efc72ac297d175b4f335a
Mr-Children-2023 <129924978+Mr-Children-2023@users.noreply.github.com>
2023-10-15 21:40:39 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,41 @@
-function comment (str:string){}
 function runsafely (speed: number) {
     respeed = Math.constrain(speed, 0, maxspeed)
-    if(BitCar.linesensor(IRLineSensor.left)&&BitCar.linesensor(IRLineSensor.right)){
-        comment("右も左にも反応しているため、直進")
+    left = BitCar.linesensor(IRLineSensor.left)
+    right = BitCar.linesensor(IRLineSensor.right)
+    if (left && right) {
+        runstraghtsafely()
+    } else if (left) {
+        move(Math.constrain(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, maxspeedturning * leftpawer), Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 3))
+    } else if (right) {
+        move(Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 3), 5 * Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 10))
+    } else {
         runstraghtsafely()
     }
-
 }
 function runstraight (speed: number) {
     respeed = Math.constrain(speed, 0, maxspeed)
-    BitCar.move(respeed, respeed)
+    move(respeed, respeed)
 }
 function runstraghtsafely () {
-    runstraight(5 + 5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
+    runstraight(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
 }
 function runstraightsafely_uselinefollow () {
-    runsafely(10 + 5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
+    runsafely(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
 }
+function comment (str: string) {
+	
+}
+function move (leftspeed: number, rightspeed: number) {
+    BitCar.move(leftpawer * leftspeed, rightpawer * rightspeed)
+}
+let right = false
+let left = false
 let respeed = 0
 let maxspeed = 0
+let leftpawer = 0
+let rightpawer = 0
+let maxspeedturning = 0
+maxspeedturning = 1
+rightpawer = 1
+leftpawer = 1
 maxspeed = 1
