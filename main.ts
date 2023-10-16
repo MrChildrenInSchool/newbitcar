aac779752bbc2d8fc8b17f2fceb974219219def2
Mr-Children-2023 <129924978+Mr-Children-2023@users.noreply.github.com>
2023-10-16 22:59:23 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
-comment("GPS機能が欲しかったの。。")
-function runsafely (speed: number) {
-    respeed = Math.constrain(speed, 0, maxspeed)
+function runsafely() {
     left = BitCar.linesensor(IRLineSensor.left)
     right = BitCar.linesensor(IRLineSensor.right)
     if (stopms > 10000) {
         avoid(["right", "left"][randint(0, 1)])
     } else {
-        if (respeed == 0) {
+        if (BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm) <= 0) {
             if (stopms == 0) {
                 stopstart = input.runningTimeMicros()
             }
@@ -25,49 +23,54 @@ function runsafely (speed: number) {
         }
     }
 }
-function runstraight (speed: number) {
+function runstraight(speed: number) {
     respeed = Math.constrain(speed, 0, maxspeed)
     move(respeed, respeed)
 }
-function comment(str:string){}
-function error(err:string){
-    comment("エラー処理")
-}
 // あとは色やれたら完成かな
 // あ、避けるのも必要だった。。
-function avoid (direction: string) {
+function avoid(direction: string) {
     comment("途中です。。")
+    distance = BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm)
     if (direction == "right") {
-    	
+            move(10,5)
+            runstraghtsafely()
+            move(5,10)
+            runstraghtsafely()
+        for(let i = 0;;){}
+
     } else if (direction == "left") {
-    	
+
     } else {
-    	comment("エラー処理")
+        comment("エラー処理")
     }
 }
-function runstraghtsafely () {
-    
+function runstraghtsafely() {
     runstraight(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
 }
-function runstraightsafely_uselinefollow () {
-    runsafely(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
+function error(err: string) {
+    comment("エラー処理")
 }
-function move (leftspeed: number, rightspeed: number) {
-    
+function comment(str: string) {
+
+}
+function move(leftspeed: number, rightspeed: number) {
     BitCar.move(leftpawer * leftspeed, rightpawer * rightspeed)
 }
-function main () {
-    runsafely(1)
+function main() {
+    runsafely()
 }
+let distance = 0
+let respeed = 0
 let stopstart = 0
 let stopms = 0
 let right = false
 let left = false
-let respeed = 0
 let maxspeed = 0
 let leftpawer = 0
 let rightpawer = 0
 let maxspeedturning = 0
+comment("GPS機能が欲しかったの。。")
 maxspeedturning = 1
 rightpawer = 1
 leftpawer = 1
