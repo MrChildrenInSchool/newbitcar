a1dec654c70faaac83820d4ee4bd3ac74e6756e7
Mr-Children-2023 <129924978+Mr-Children-2023@users.noreply.github.com>
2023-10-16 19:05:46 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,66 @@
-// あとは色やれたら完成かな
-// あ、避けるのも必要だった。。
+comment("GPS機能が欲しかったの。。")
 function runsafely (speed: number) {
     respeed = Math.constrain(speed, 0, maxspeed)
     left = BitCar.linesensor(IRLineSensor.left)
     right = BitCar.linesensor(IRLineSensor.right)
-    // ウインカー
-    if (left && right) {
-        runstraghtsafely()
-    } else if (left) {
-        move(Math.constrain(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, maxspeedturning * leftpawer), Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 3))
-    } else if (right) {
-        move(Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 3), 5 * Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, maxspeedturning * rightpawer))
+    if (stopms > 10000) {
+        avoid(["right", "left"][randint(0, 1)])
     } else {
-        runstraghtsafely()
+        if (respeed == 0) {
+            if (stopms == 0) {
+                stopstart = input.runningTimeMicros()
+            }
+            stopms = input.runningTimeMicros() - stopstart
+        } else {
+            stopms = 0
+            if (left && right) {
+                runstraghtsafely()
+            } else if (left) {
+                move(Math.constrain(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, maxspeedturning * leftpawer), Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 3))
+            } else if (right) {
+                move(Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, 3), 5 * Math.constrain(BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm), 0, maxspeedturning * rightpawer))
+            } else {
+                runstraghtsafely()
+            }
+        }
     }
 }
 function runstraight (speed: number) {
     respeed = Math.constrain(speed, 0, maxspeed)
     move(respeed, respeed)
 }
+function comment(str:string){}
+function error(err:string){
+    comment("エラー処理")
+}
+// あとは色やれたら完成かな
+// あ、避けるのも必要だった。。
+function avoid (direction: string) {
+    comment("途中です。。")
+    if (direction == "right") {
+    	
+    } else if (direction == "left") {
+    	
+    } else {
+    	comment("エラー処理")
+    }
+}
 function runstraghtsafely () {
+    
     runstraight(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
 }
 function runstraightsafely_uselinefollow () {
     runsafely(5 * BitCar.grove_ultrasonic_v2(GrovePin.P12, DistanceUnit.cm))
 }
 function move (leftspeed: number, rightspeed: number) {
+    
     BitCar.move(leftpawer * leftspeed, rightpawer * rightspeed)
 }
 function main () {
     runsafely(1)
 }
+let stopstart = 0
+let stopms = 0
 let right = false
 let left = false
 let respeed = 0
